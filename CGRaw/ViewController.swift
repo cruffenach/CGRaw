fa801e5bf7f5fab3101336615931a65f37a03c4c
Collin Ruffenach <collin@simple.com>
2014-10-18 10:56:22 -0700

    Change demo to noise

--- CGRaw/ViewController.swift
+++ CGRaw/ViewController.swift
@@ -18,20 +18,20 @@ class ColorView : UIView {
                 let modi : Bool = (i % 2 == 1)
                 let modj : Bool = (j % 2 == 1)
                 let hue : CGFloat = CGFloat(i)/CGRectGetWidth(rawBounds)
-                let color = UIColor(
-                    hue: CGFloat(j)/CGRectGetHeight(rawBounds),
-                    saturation: CGFloat(i)/CGRectGetWidth(rawBounds),
-                    brightness: CGFloat((i+j))/(CGRectGetWidth(rawBounds)+CGRectGetHeight(rawBounds)),
-                    alpha: CGFloat(1.0))
-//                let random : () -> CGFloat = {
-//                    return CGFloat((CGFloat(arc4random()) % 100.0)/100.0)
-//                }
-//                let color = UIColor (
-//                    hue: random(),
-//                    saturation: random(),
-//                    brightness: random(),
-//                    alpha: random()
-//                )
+//                let color = UIColor(
+//                    hue: CGFloat(j)/CGRectGetHeight(rawBounds),
+//                    saturation: CGFloat(i)/CGRectGetWidth(rawBounds),
+//                    brightness: CGFloat((i+j))/(CGRectGetWidth(rawBounds)+CGRectGetHeight(rawBounds)),
+//                    alpha: CGFloat(1.0))
+                let random : () -> CGFloat = {
+                    return CGFloat((CGFloat(arc4random()) % 100.0)/100.0)
+                }
+                let color = UIColor (
+                    hue: random(),
+                    saturation: random(),
+                    brightness: random(),
+                    alpha: random()
+                )
                 color.setFill()
                 UIRectFill(CGRaw.uiKitRect(
                     rawRect: CGRect(
